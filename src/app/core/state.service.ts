e99300f3eabf68e98b447c6c7151f5a18a5d1af1
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-16 14:50:07 +0000

    Add display side nav property

--- src/app/core/state.service.ts
+++ src/app/core/state.service.ts
@@ -2,6 +2,25 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class StateService {
+    private displaySideNav!: boolean;
 
-    constructor() { }
+    constructor() {
+        this.displaySideNav = false;
+    }
+
+    isSideNavShown(): boolean {
+        return this.displaySideNav;
+    }
+
+    hideSideNav(): void {
+        this.displaySideNav = false;
+    }
+
+    showSideNav(): void {
+        this.displaySideNav = true;
+    }
+
+    toggleSideNav(): void {
+        this.displaySideNav = !this.displaySideNav;
+    }
 }
