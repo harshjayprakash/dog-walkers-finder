61a2f7f31b741a71b3152c25867abd8a766d5218
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-16 14:49:51 +0000

    Create tests

--- src/app/core/state.service.spec.ts
+++ src/app/core/state.service.spec.ts
@@ -1,8 +1,7 @@
 import { TestBed } from '@angular/core/testing';
-
 import { StateService } from './state.service';
 
-describe('StateService', () => {
+describe('CoreModule::StateService', () => {
     let service: StateService;
 
     beforeEach(() => {
@@ -12,5 +11,24 @@ describe('StateService', () => {
 
     it('should be created', () => {
         expect(service).toBeTruthy();
+    });
+
+    it(`'displaySideNav' should have default value of false`, () => {
+        expect(service.isSideNavShown()).toBeFalse();
+    });
+
+    it(`'displaySideNav' toggle should change value to true`, () => {
+        service.toggleSideNav();
+        expect(service.isSideNavShown()).toBeTrue();
+    });
+
+    it(`'set 'displaySideNav' to false`, () => {
+        service.hideSideNav();
+        expect(service.isSideNavShown()).toBeFalse();
+    });
+
+    it(`set 'displaySideNav' to true`, () => {
+        service.showSideNav();
+        expect(service.isSideNavShown()).toBeTrue();
     });
 });
