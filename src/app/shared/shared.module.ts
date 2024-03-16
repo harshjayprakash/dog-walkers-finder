b1f0458bdaec1618967f4278868ee8a4e7a73525
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-16 14:51:09 +0000

    Update imports and providers

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
+import { StateService } from '../core/state.service';
+import { MatCommonModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { CoreModule } from '../core/core.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
@@ -9,7 +18,17 @@ import { SideNavComponent } from './side-nav/side-nav.component';
         SideNavComponent
     ],
     imports: [
-        CommonModule
-    ]
+        CommonModule,
+        RouterModule,
+        CoreModule,
+        MatCommonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatListModule
+    ],
+    exports: [ToolbarComponent, SideNavComponent],
+    providers: [StateService]
 })
 export class SharedModule { }
