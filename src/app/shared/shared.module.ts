fd1e321a72331a54bf049f11acd1d1db94dc74e4
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-16 13:40:36 +0000

    Generate side-nav component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
     declarations: [
-        ToolbarComponent
+        ToolbarComponent,
+        SideNavComponent
     ],
     imports: [
         CommonModule
