220163237861a40c4461e984c8287904fb4b2ec6
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-16 14:52:38 +0000

    Add toolbar events

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StateService } from '../../core/state.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-toolbar',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
     styleUrl: './toolbar.component.css'
 })
 export class ToolbarComponent {
+
+    constructor(
+        private router: Router,
+        private state: StateService) { }
+
+    onAccountButtonClick(event: any): void {
+        this.router.navigateByUrl('/account');
+    }
+
+    onMenuButtonClick(event: any): void {
+        this.state.toggleSideNav();
+    }
 
 }
