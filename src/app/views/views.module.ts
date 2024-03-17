b91a342f9fd1216f3dbc9600b474f0d584fe4ba8
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-17 13:39:57 +0000

    Add routes

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+export const VIEWS_MODULE_ROUTES: Routes = [
+    {
+        path: "",
+        title: "Home - Waqq.ly",
+        component: HomeComponent
+    }
+];
 
 @NgModule({
     declarations: [
         HomeComponent
     ],
     imports: [
-        CommonModule
-    ]
+        CommonModule,
+        RouterModule.forChild(VIEWS_MODULE_ROUTES)
+    ],
+    exports: [RouterModule]
 })
 export class ViewsModule { }
