8a2a68937c8b38ef1a54336316d98d9a0343c310
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-18 12:38:09 +0000

    Import view module routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewsModule } from './views/views.module';
 
-export const routes: Routes = [];
+
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot([
+        { path: "", loadChildren: () => ViewsModule }
+    ])],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
