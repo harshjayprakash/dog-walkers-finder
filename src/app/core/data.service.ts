f810f6cc7f41eb85eca15766979c933d77110fa1
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-18 12:39:43 +0000

    Create get users function

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,7 +1,19 @@
-import { Injectable } from '@angular/core';
+import { get, put } from 'aws-amplify/api';
 
 @Injectable()
 export class DataService {
 
-    constructor() { }
+    async getUsers(): Promise<any> {
+        try {
+            const restOperation = await get({
+                apiName: 'usersApi',
+                path: '/users'
+            });
+            const response = await restOperation.response;
+            return response.body.json();
+        }
+        catch (err) {
+            console.log('Failed to retrieve users.');
+        }
+    }
 }
