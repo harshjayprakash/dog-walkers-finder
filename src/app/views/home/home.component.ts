663a721ee08308bc695e045c0a4025fd7cbab3e7
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-18 12:44:07 +0000

    Inject data service

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../core/data.service';
 
 @Component({
     selector: 'app-home',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
     styleUrl: './home.component.css'
 })
 export class HomeComponent {
+    constructor(private data: DataService) {}
 
 }
