63d60e63990c5e0eb7425df7fe1ccbff48c14119
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-21 10:18:05 +0000

    Move

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Inject, Injectable, LOCALE_ID } from '@angular/core';
 import { formatDate } from '@angular/common';
 import { get, put } from 'aws-amplify/api';
+import { AES } from 'crypto-js'
 
 @Injectable()
 export class DataService {
@@ -18,10 +19,14 @@ export class DataService {
         catch (err) { console.log('Failed to retrieve users.'); }
     }
 
-    async AddUser() {
+    async AddUser(): boolean {
         let idString: string = formatDate(
             Date.now(), 'YYYYMMddTHHmmssSSS', this.locale.toString());
-
-
+        try {
+            const restOperation = await put(
+                { apiName }
+            );
+        }
+        catch (err) { console.log('Failed to add user.') }
     }
 }
