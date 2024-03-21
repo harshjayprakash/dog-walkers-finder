4f8c01d595a4ce126a7902a19b686cdbf707deac
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-21 10:40:30 +0000

    Move logic

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,32 +1,8 @@
-import { Inject, Injectable, LOCALE_ID } from '@angular/core';
-import { formatDate } from '@angular/common';
-import { get, put } from 'aws-amplify/api';
-import { AES } from 'crypto-js'
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataService {
 
-    constructor(@Inject(LOCALE_ID) private locale: String) { }
+    constructor() { }
 
-    async getUsers(): Promise<any> {
-        try {
-            const restOperation = await get(
-                { apiName: 'usersApi', path: '/users' }
-            );
-            const response = await restOperation.response;
-            return response.body.json();
-        }
-        catch (err) { console.log('Failed to retrieve users.'); }
-    }
-
-    async AddUser(): boolean {
-        let idString: string = formatDate(
-            Date.now(), 'YYYYMMddTHHmmssSSS', this.locale.toString());
-        try {
-            const restOperation = await put(
-                { apiName }
-            );
-        }
-        catch (err) { console.log('Failed to add user.') }
-    }
 }
--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,7 +1,43 @@
-import { Injectable } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Inject, Injectable, LOCALE_ID } from '@angular/core';
+import { get, put } from 'aws-amplify/api';
+import { SHA256 } from 'crypto-js';
 
 @Injectable()
 export class UsersService {
+    private apiDetails = {
+        apiName: 'usersApi',
+        path: '/users'
+    };
 
-    constructor() { }
+    constructor(@Inject(LOCALE_ID) private locale: String) { }
+
+    async getUsers(): Promise<any> {
+        try {
+            const restOperation = await get(
+                { ...this.apiDetails }
+            );
+            const response = await restOperation.response;
+            console.log('Amplify API GET Succeeded: ', response);
+            console.log(response.body.json());
+            return response.body.json();
+        }
+        catch (err) { console.log('Failed to retrieve users.'); }
+    }
+
+    async addUser(username: string, password: string): Promise<void> {
+        let idString: string = formatDate(
+            Date.now(), 'YYYYMMddTHHmmssSSS', this.locale.toString());
+        try {
+            const restOperation = await put({
+                ...this.apiDetails,
+                options: { body:
+                    { id: idString, username, password: SHA256(password).toString() }
+                }
+            });
+            const response = await restOperation.response;
+            console.log('Amplify API PUT Succeeded: ', response);
+        }
+        catch (err) { console.log('Failed to add user.'); }
+    }
 }
