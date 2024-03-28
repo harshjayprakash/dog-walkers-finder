a23801520a4beedaed2c74ce831a12a4aa45c822
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-03-28 11:40:27 +0000

    Inject auth service

--- src/app/views/find-walkers/find-walkers.component.ts
+++ src/app/views/find-walkers/find-walkers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
     selector: 'app-find-walkers',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
     styleUrl: './find-walkers.component.css'
 })
 export class FindWalkersComponent {
+
+    constructor (public auth: AuthService) { }
 
 }
