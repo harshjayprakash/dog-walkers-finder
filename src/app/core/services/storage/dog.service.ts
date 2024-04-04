4e09591f4cbfa24bde015444cde68041aa042581
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-04-04 21:33:30 +0100

    Extend dynamo service

--- src/app/core/services/storage/dog.service.ts
+++ src/app/core/services/storage/dog.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
+import { DynamoService } from './dynamo.service';
 
 @Injectable()
-export class DogService {
-    private apiDetails = {
-        apiName: 'dogsApi',
-        path: '/users'
+export class DogService extends DynamoService {
+
+    constructor() {
+        super();
+        this.apiDetails.apiName = 'dogsApi';
+        this.apiDetails.path = '/dogs'
     }
-
-
-    constructor() { }
 }
