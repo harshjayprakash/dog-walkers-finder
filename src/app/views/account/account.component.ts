3550560396e82480db85ed0e19384893f3152f23
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-04-04 13:45:39 +0100

    Implement action functions

--- src/app/views/account/account.component.ts
+++ src/app/views/account/account.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/services/auth.service';
+import { UsersService } from '../../core/services/storage/users.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-account',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
     styleUrl: './account.component.css'
 })
 export class AccountComponent {
+    userId?: string;
+    username?: string;
+    name?: string;
+    postalCode?: string;
+    dogWalker?: boolean;
+
+
+    constructor(
+        private auth: AuthService,
+        private users: UsersService,
+        private router: Router
+    ) {
+        auth.updateUser();
+        this.userId = auth.getUserId();
+    }
+
+    updateUserInformation(): void {
+        this.users.addUser(
+            this.userId,
+            this.username || 'not specified',
+            this.postalCode || 'not specified',
+            this.dogWalker || false,
+            []
+        );
+    }
+
+    logOut(): void {
+        this.auth.logOutUser();
+        this.auth.updateUser();
+        this.router.navigateByUrl('/');
+        window.location.reload();
+    }
 
 }
