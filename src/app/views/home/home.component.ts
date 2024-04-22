c0207469d5bafa3ae3f7b8b2568d19e1bc416c42
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-04-22 13:57:15 +0100

    Add get dogs check

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DogService } from '../../core/services/storage/dog.service';
 import { UsersService } from '../../core/services/storage/users.service';
 
 @Component({
@@ -8,10 +9,13 @@ import { UsersService } from '../../core/services/storage/users.service';
 })
 export class HomeComponent {
 
-    constructor(private users: UsersService) {}
+    constructor(private users: UsersService, private dogs: DogService) {}
 
     onClick() {
         this.users.getUsers().then(result => {
+            console.log(result);
+        }).catch(err => console.log(err));
+        this.dogs.getDogs().then(result => {
             console.log(result);
         }).catch(err => console.log(err));
     }
