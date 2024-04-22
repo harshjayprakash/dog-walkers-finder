97133804aa6aff55712fc48242cdde6e6fc2c13d
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-04-22 10:48:29 +0100

    Add navigate and reload

--- src/app/views/cognito-auth/cognito-auth.component.ts
+++ src/app/views/cognito-auth/cognito-auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-cognito-auth',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
     styleUrl: './cognito-auth.component.css'
 })
 export class CognitoAuthComponent {
+
+    constructor(private router: Router) { }
+
+    reload() {
+        window.location.reload();
+        this.router.navigateByUrl('/account');
+    }
 
 }
