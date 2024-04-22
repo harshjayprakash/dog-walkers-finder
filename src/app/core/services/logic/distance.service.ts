fa5328117d757b0ca36ebd25f5c97146919bdbe5
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-04-22 10:32:21 +0100

    Inject http client

--- src/app/core/services/logic/distance.service.ts
+++ src/app/core/services/logic/distance.service.ts
@@ -1,7 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DistanceService {
 
-  constructor() { }
+    constructor(private http: HttpClient) { }
+
 }
