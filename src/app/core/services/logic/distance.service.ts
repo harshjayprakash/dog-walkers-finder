6ac8d67e28136b1e222a0df473ac36cca694298c
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-05-02 11:15:07 +0100

    Update postcode validality function

--- src/app/core/services/logic/distance.service.ts
+++ src/app/core/services/logic/distance.service.ts
@@ -1,13 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, tap, throwError } from 'rxjs';
+import { PostcodeApiValidity } from '../../model/postcode';
 
 @Injectable()
 export class DistanceService {
 
     constructor(private http: HttpClient) { }
 
-    isValidPostalCode(postalCode: string) {
-        this.http.get(`api.postalcode.io/postcodes/${postalCode}/validate`);
+    isValidPostCode(postalCode: string) {
+        return this.http.get<PostcodeApiValidity>(`https://api.postcodes.io/postcodes/${postalCode}/validate`)
+            .pipe(
+                tap(result => {
+
+                }),
+                catchError(error => {
+                    return throwError(() => error);
+                })
+            )
+
     }
 };
 
