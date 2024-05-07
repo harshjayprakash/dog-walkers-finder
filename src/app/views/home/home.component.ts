d64fd3852d0ff3b7c4d2861d31a54a06d63175a1
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-05-07 14:08:44 +0100

    Remove all functionality

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { DistanceService } from '../../core/services/logic/distance.service';
-import { DogService } from '../../core/services/storage/dog.service';
-import { UsersService } from '../../core/services/storage/users.service';
 
 @Component({
     selector: 'app-home',
@@ -10,22 +7,7 @@ import { UsersService } from '../../core/services/storage/users.service';
 })
 export class HomeComponent {
 
-    constructor(
-        private users: UsersService,
-        private dogs: DogService,
-        private distance: DistanceService
-    ) {}
-
-    onClick() {
-        let ret = this.distance.isValidPostCode("so158qz")
-            .subscribe(
-                result => {
-
-                },
-                error => {
-
-                }
-            )
-    }
+    constructor() {}
 
 }
+
