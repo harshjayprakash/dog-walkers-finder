1d24e933ab81b6ccd0d2a0b91b6586772c00d51e
Harsh Jayprakash <harshjayprakash@outlook.com>
2024-05-07 14:07:11 +0100

    Update postcode api models

--- src/app/core/model/postcode.ts
+++ src/app/core/model/postcode.ts
@@ -1,49 +1,65 @@
 export interface PostcodeApiResponse {
     status: number;
-    result: {
-        postcode: string;
-        quality: number;
-        eastings: number | null;
-        northings: number | null;
-        country: string;
-        nhs_ha: string | null;
-        date_of_introduction: string;
-        longitude: number | null;
-        latitude: number | null;
-        european_electoral_region: string | null;
-        primary_care_trust: string | null;
-        region: string | null;
+    result: PostcodeApiInformation
+}
+
+export interface PostcodeApiInformation {
+    postcode: string;
+    quality: number;
+    eastings: number | null;
+    northings: number | null;
+    country: string;
+    nhs_ha: string | null;
+    date_of_introduction: string;
+    longitude: number | null;
+    latitude: number | null;
+    european_electoral_region: string | null;
+    primary_care_trust: string | null;
+    region: string | null;
+    lsoa: string | null;
+    msoa: string | null;
+    incode: string;
+    outcode: string;
+    parliamentary_constituency: string | null;
+    parliamentary_constituency_2024: string | null;
+    admin_district: string | null;
+    parish: string | null;
+    admin_county: string | null;
+    admin_ward: string | null;
+    ced: string | null;
+    ccg: string | null;
+    nuts: string | null;
+    pfa: string | null;
+    codes: {
+        admin_district: string;
+        admin_county: string;
+        admin_ward: string;
+        parish: string;
+        parliamentary_constituency: string;
+        parliamentary_constituency_2024: string;
+        ccg: string;
+        ccg_id: string;
+        ced: string;
+        nuts: string;
         lsoa: string | null;
         msoa: string | null;
-        incode: string;
-        outcode: string;
-        parliamentary_constituency: string | null;
-        parliamentary_constituency_2024: string | null;
-        admin_district: string | null;
-        parish: string | null;
-        admin_county: string | null;
-        admin_ward: string | null;
-        ced: string | null;
-        ccg: string | null;
-        nuts: string | null;
+        lau2: string | null;
         pfa: string | null;
-        codes: {
-            admin_district: string;
-            admin_county: string;
-            admin_ward: string;
-            parish: string;
-            parliamentary_constituency: string;
-            parliamentary_constituency_2024: string;
-            ccg: string;
-            ccg_id: string;
-            ced: string;
-            nuts: string;
-            lsoa: string | null;
-            msoa: string | null;
-            lau2: string | null;
-            pfa: string | null;
-        };
-    }
+    };
+}
+
+export interface PostcodeApiBulkGeolocation {
+    query: {
+        longitude: number | null,
+        latitude: number | null,
+        radius: number | null
+    },
+    result: PostcodeApiInformation[];
+}
+
+export interface PostcodeApiBulkReverseGeocoding {
+    status: number,
+    result: PostcodeApiBulkGeolocation[];
 }
 
 export interface PostcodeApiValidity {
@@ -52,18 +68,15 @@ export interface PostcodeApiValidity {
 }
 
 export interface PostcodeApiOutcode {
-    status: number;
-    result: {
-        outcode: string;
-        longitude: number | null;
-        latitude: number | null;
-        northings: number;
-        eastings: number;
-        admin_district: string[];
-        parish: string[];
-        admin_county: string[];
-        admin_ward: string[];
-        country: string[];
-        parliamentary_constituency: string[];
-    }
+    outcode: string;
+    longitude: number | null;
+    latitude: number | null;
+    northings: number;
+    eastings: number;
+    admin_district: string[];
+    parish: string[];
+    admin_county: string[];
+    admin_ward: string[];
+    country: string[];
+    parliamentary_constituency: string[];
 }
